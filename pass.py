eb8baf3f6811a3b0d3da8283ca1316b28ed9dec0
Guilherme Santos <guipinela.santos@gmail.com>
2021-03-07 16:09:54 +0000

    atualização

--- pass.py
+++ pass.py
@@ -1,7 +1,8 @@
 import random
+escolha = ''
 letras = '123456789qwertyuiopasdfghjklçzxcvbnm,.-:;<>/()=?«»!#$%&^~+*'
 tamanho_pass = int(input('Qual tamanho deseja para a sua passe? '))
+print('Sua passe é a seguinte: ', end='')
 for l in range(0, tamanho_pass):
     escolha = random.choice(letras)
-    for l in escolha:
-        print(l, end='')
+    print(escolha, end='')
